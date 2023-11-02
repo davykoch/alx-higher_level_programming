347cbd503fc34524a4565d821fb858fcec2de9d2
davykoch <koechdavis24@gmail.com>
2023-11-02 15:42:49 +0300

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
+
 def islower(c):
-
-    return 'a' <= c <= 'z'
-
-
-if __name__ == "__main__":
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False

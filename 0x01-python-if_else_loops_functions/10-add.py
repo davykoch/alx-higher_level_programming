a38db660c770ea34899e7c198a39f4a6e9a33e7d
davykoch <koechdavis24@gmail.com>
2023-11-02 14:01:09 +0300

    a + b

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Return the addition of two numbers."""
+    return a + b
+
+
+if _name_ == "_main_":
+    print(add(1, 2))
+    print(add(98, 0))
+    print(add(100, -2))

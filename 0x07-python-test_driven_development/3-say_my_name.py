4bb37b9c2bbd34afeb3e7c7a0e4ab2dc50763085
davykoch <koechdavis24@gmail.com>
2023-12-01 02:42:15 +0300

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,9 +3,10 @@
 say_my_name - prints "My name is <first name> <last name>"
 
 first_name and last_name must be strings, otherwise raise a TypeError
-exception with the message first_name must be a string or last_name must be a string
+exception with the message first_name/last_name must be a string
 You are not allowed to import any module
 """
+
 
 def say_my_name(first_name, last_name=""):
     """

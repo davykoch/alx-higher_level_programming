7ed9d6ad54d8b729999863760dc8d459b232bb0d
davykoch <koechdavis24@gmail.com>
2023-12-01 00:36:58 +0300

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""function that adds 2 integers"""
+"""Function that adds 2 integers"""
 
 
 def add_integer(a, b=98):
-	"""add integers a and b with conditions
-	must be integers or floats
-	must be first casted to integers if they are float
-	"""
-	if not isinstance(a, (int, float)) or a is None:
+    """Add integers a and b with conditions:
+    - Must be integers or floats
+    - Must be first casted to integers if they are float"""
+    if not isinstance(a, (int, float)) or a is None:
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")

c8edd6edab43733d6c4affb3d176164cb7b739e3
davykoch <koechdavis24@gmail.com>
2023-12-01 15:02:51 +0300

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""
-say_my_name - prints "My name is <first name> <last name>"
-
-first_name and last_name must be strings, otherwise raise a TypeError
-exception with the message first_name/last_name must be a string
-You are not allowed to import any module
-"""
-
 
 """
 Module for say_my_name function.

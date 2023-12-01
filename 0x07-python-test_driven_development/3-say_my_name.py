0166562e41f84b5fd0b045303d47e81427d12f4c
davykoch <koechdavis24@gmail.com>
2023-12-01 15:01:18 +0300

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,6 +8,11 @@ You are not allowed to import any module
 """
 
 
+"""
+Module for say_my_name function.
+"""
+
+
 def say_my_name(first_name, last_name=""):
     """
     Prints "My name is <first name> <last name>".

74a8d286d6b8883951714adf0b83fcd4b532dcad
davykoch <koechdavis24@gmail.com>
2023-12-12 02:20:43 +0300

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,11 +3,12 @@
 
 
 class Base:
-	"""the base class with private class attribute"""
-	__nb_objects = 0
-	def __init__(self, id=None):
-		if id is not None:
-		    self.id = id
-		else:
-		    Base.__nb_objects += 1
-		    self.id = Base.__nb_objects
+    """the base class with private class attribute"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

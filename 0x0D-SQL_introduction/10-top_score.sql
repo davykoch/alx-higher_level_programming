17f8a41306767b867a17edd8239f894ad785b0a6
davykoch <koechdavis24@gmail.com>
2024-01-19 04:52:55 +0300

    List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
 -- Results should display both the score and the name (in this order)
 -- Records should be ordered by score (top first)
-SHOW CREATE TABLE second_table
-ORDERED BY score DESC, name;
+SELECT score, name FROM second_table ORDER BY score DESC;

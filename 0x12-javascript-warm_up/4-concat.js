39f6a9210218134dd05308df2a336b480160a783
davykoch <koechdavis24@gmail.com>
2024-02-16 01:28:45 +0300

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-// Extract the two arguments using process.argv
-const argument1 = process.argv[2] || 'undefined';
-const argument2 = process.argv[3] || 'undefined';
-
-console.log(`${argument1}  is ${argument2}`);
+console.log(`${process.argv[2] || 'undefined'} is ${process.argv[3] || 'undefined'}`);

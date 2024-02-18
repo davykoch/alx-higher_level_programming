468719e2e3f74f9e9b08b6b6297cb51e776b6db6
davykoch <koechdavis24@gmail.com>
2024-02-19 01:36:50 +0300

    SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Takes in arguments and displays all values in the states table
+where name matches the argument (safe from MySQL injection).
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    database = sys.argv[3]
+    user = sys.argv[1]
+    password = sys.argv[2]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (state_name,))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()

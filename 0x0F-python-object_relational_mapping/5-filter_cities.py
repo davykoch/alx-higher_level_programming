b2d814bd59d0eeb01ef3bcae851c9b95f100a632
davykoch <koechdavis24@gmail.com>
2024-02-19 01:57:54 +0300

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -27,10 +27,10 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = "SELECT cities.name FROM cities\
-                 LEFT JOIN states\
-                 ON states.id = cities.state_id\
-                 WHERE states.name LIKE BINARY (%s) ORDER BY id ASC"
+    query = "SELECT cities.name FROM cities \
+             LEFT JOIN states ON cities.state_id = states.id \
+             WHERE states.name = %s \
+             ORDER BY cities.id ASC"
     cursor.execute(query, (state_name,))
 
     rows = cursor.fetchall()

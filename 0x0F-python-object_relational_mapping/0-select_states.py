9c4ee38789685aceda7fdd74d25335107be09ad4
davykoch <koechdavis24@gmail.com>
2024-02-18 23:53:15 +0300

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 """
 lists all states from the database hbtn_0e_0_usa:
-takes 3 arguments:mysql username, mysql password and database name
+takes 3 arguments: mysql username, mysql password, and database name
 """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    db_name = input("Enter database name: ")
-    user = input("Enter username: ")
-    password = input("Enter password: ")
+    db_name = sys.argv[3]
+    user = sys.argv[1]
+    password = sys.argv[2]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=db_name
+    )
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC;")
     states = cursor.fetchall()
+
+    for state in states:
+        print(state)
 
     cursor.close()
     db.close()

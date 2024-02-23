a7f4d0100895cdbc63cb8bc742b806157e1f55e9
davykoch <koechdavis24@gmail.com>
2024-02-24 01:19:22 +0300

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,8 +11,8 @@ from model_city import City
 
 if __name__ == "__main__":
     engine = create_engine(
-        f'mysql+mysqldb: // {sys.argv[1]}:
-        {sys.argv[2]}@localhost: 3306/{sys.argv[3]}',
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True
     )
     Base.metadata.create_all(engine)

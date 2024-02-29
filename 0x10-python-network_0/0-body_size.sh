74e660a5500241f76f48da5a4e0c174def942642
davykoch <koechdavis24@gmail.com>
2024-03-01 02:38:59 +0300

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes a URL as input, sends a request using curl, and displays the size of the response body in bytes.
-
 curl -sI "$1" |  grep Content-Length | cut -d ":" -f 2 | cut -d " " -f 2

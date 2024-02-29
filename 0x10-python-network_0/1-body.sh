7f06a8d36211c3e662919858eb22ee014ce5e2f4
davykoch <koechdavis24@gmail.com>
2024-03-01 02:49:25 +0300

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script Description: Sends a GET request to a URL using curl and displays the body of the response for a 200 status code.
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s "$1"
+curl -sL "$1"

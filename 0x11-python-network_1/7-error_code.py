d6e6158c9354c3aeb9fc8e57bacc7aba209834e8
davykoch <koechdavis24@gmail.com>
2024-03-02 03:51:43 +0300

    error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,12 +7,10 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    try:
-        response = requests.get(url)
 
-        if response.status_code >= 400:
-            print(f"Error code:", response.status_code)
-        else:
-            print(response)
-    except requests.RequestException as e:
-        print(f"Error making the request: {e}")
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print(f"Error code:", response.status_code)
+    else:
+        print(response.text)
